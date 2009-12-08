c6fd14940aa7e91d9e134abba8f0a5e47198f91a
mihey81 <mihey81@localhost>
2009-12-08 21:00:16 +0100

    Reviewed Interface; introduced draw_tiles method

--- display.rb
+++ display.rb
@@ -6,30 +6,41 @@
 require 'gosu'
 
 class Interface
-	def initialize(tileset)
-		@tileset=tileset
+	def Interface.set_tileset(tileset)
+		@@tileset=tileset
 	end
+	
+	def Interface.draw_tiles(x,y,content,direction=:horizontal,color=0xFFFFFFFF)
+		#expected argument must be either a Symbol or an Array of symbols
+		# Draws either the one tile or a number of tiles into given direction
+		if content.class==Symbol then
+			@@tileset[content].draw(x*16,y*16,0,1,1,color)
+		elsif content.class==Array and direction==:horizontal then
+			content.length.times {|i| @@tileset[content[i]].draw((x+i)*16,y*16,0,1,1,color)}
+		elsif content.class==Array and direction==:vertical then
+			content.length.times {|j| @@tileset[content[j]].draw(x*16,(y+j)*16,0,1,1,color)}
+		end
+	end
+	
 	# draw a rectangular frame
-	def frame(x,y,width,height,type=:double,color=0xFFFFFFFF)
+	def Interface.draw_frame(x,y,width,height,type=:double,color=0xFFFFFFFF)
 		case type
 			when :double then
 				tiles={:topleft => :table_topleft_double, :topright => :table_topright_double, :bottomright => :table_bottomright_double, :bottomleft => :table_bottomleft_double, :horizontal => :table_horizontal_double, :vertical => :table_vertical_double}
 			when :single then
 				tiles={:topleft => :table_topleft_single, :topright => :table_topright_single, :bottomright => :table_bottomright_single, :bottomleft => :table_bottomleft_single, :horizontal => :table_horizontal_single, :vertical => :table_vertical_single} 
 			when :solid then 
-				tiles={:topleft => :fill100, :topright => :fill100, :bottomright => :fill100, :bottomleft => :fill100, :horizontal => :fill100, :vertical => :fill100} 
+				tiles={:topleft => :fill100, :topright => :fill100, :bottomright => :fill100, :bottomleft => :fill100, :horizontal => :fill100, :vertical => :fill100}
+			when :heart then
+				tiles={:topleft => :heart, :topright => :heart, :bottomright => :heart, :bottomleft => :heart, :horizontal => :heart, :vertical => :heart}
 		end
-		@tileset[tiles[:topleft]].draw(x*16,y*16,0,1,1,color)
-		@tileset[tiles[:topright]].draw((x+width-1)*16,y*16,0,1,1,color)
-		@tileset[tiles[:bottomright]].draw((x+width-1)*16,(y+height-1)*16,0,1,1,color)
-		@tileset[tiles[:bottomleft]].draw(x*16,(y+height-1)*16,0,1,1,color)
-		(width-2).times do |i| #draw horizontal borders; since width is a total, we substract 1
-			@tileset[tiles[:horizontal]].draw((x+i+1)*16,y*16,0,1,1,color)
-			@tileset[tiles[:horizontal]].draw((x+i+1)*16,(y+height-1)*16,0,1,1,color)
-			end
-		(height-2).times do |j| #draw vertical borders
-			@tileset[tiles[:vertical]].draw(x*16,(y+1+j)*16,0,1,1,color)
-			@tileset[tiles[:vertical]].draw((x+width-1)*16,(y+1+j)*16,0,1,1,color)
-			end
+		draw_tiles(x,y,tiles[:topleft],:horizontal,color)
+		draw_tiles(x+width-1,y,tiles[:topright],:horizontal,color)
+		draw_tiles(x+width-1,y+height-1,tiles[:bottomright],:horizontal,color)
+		draw_tiles(x,y+height-1,tiles[:bottomleft],:horizontal,color)
+		draw_tiles(x+1,y,[tiles[:horizontal]]*(width-2),:horizontal,color)
+		draw_tiles(x+1,y+height-1,[tiles[:horizontal]]*(width-2),:horizontal,color)
+		draw_tiles(x,y+1,[tiles[:vertical]]*(height-2),:vertical,color)
+		draw_tiles(x+width-1,y+1,[tiles[:vertical]]*(height-2),:vertical,color)
 	end
 end
--- main.rb
+++ main.rb
@@ -5,7 +5,6 @@ require 'tileset'
 require 'cut' # various snippets such as the new rand()
 
 class GameWindow < Gosu::Window
-
 # advanced initialization, not really needed now
 #  def initialize(ini)
 #    x=ini['Display']['DisplayX']
@@ -13,18 +12,26 @@ class GameWindow < Gosu::Window
 #    z=ini['Display']['Fullscreen']
 #    super(x.to_i,y.to_i,z.to_i)
   def initialize
-    super(800,600,0)
+    super(1024,768,0)
     self.caption="Generic title"
     @tileset=Tileset.new(self)
-    @interface=Interface.new(@tileset)
+    Interface.set_tileset(@tileset)
+    @color=Gosu::Color.new(0xFFFFFFFF)
   end
   
   def update()
+	@timer=Gosu::milliseconds()/5
+	@color=Gosu::Color.new(255,0,@timer%255,0)
 
   end
   
   def draw()
-	  @interface.frame(0,0,width/16,height/16,:solid,0xFF888888) #this draws a neat border along screen edges
+	  Interface.draw_frame(0,0,width/32,height/16,:double,0xFF00FFFF)
+	  Interface.draw_frame(32,0,32,10,:single,0xFFFFFFFF)
+	  Interface.draw_frame(32,10,32,10,:solid,0xFF0000FF)
+	  Interface.draw_frame(32,20,32,10,:heart,0xFFFF0000)
+	  Interface.draw_tiles(33,11,[:H,:e,:l,:l,:o,:space,:w,:o,:r,:l,:d],:horizontal,0xFFCCCCCC)
+	  Interface.draw_tiles(33,31,[:H,:e,:l,:l,:o,:space,:w,:o,:r,:l,:d],:vertical,@color)
   end
 end
 
