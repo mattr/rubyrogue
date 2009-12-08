aa06dbbb087aa6d9c9470153e39fa7228fe5dff5
mihey81 <mihey81@localhost>
2009-12-09 00:13:59 +0100

    Additional changes to Interface (Z ordering etc.)

--- display.rb
+++ display.rb
@@ -6,20 +6,24 @@
 require 'gosu'
 
 module Interface
-	def draw_tiles(x,y,content,direction=:horizontal,color=0xFFFFFFFF)
+	def draw_tiles(x,y,z_order,content,direction=:horizontal,color=0xFFFFFFFF)
 		#expected argument must be either a Symbol or an Array of symbols
 		# Draws either the one tile or a number of tiles into given direction
 		if content.class==Symbol then
-			@tileset[content].draw(x*16,y*16,0,1,1,color)
+			@tileset[content].draw(x*16,y*16,z_order,1,1,color)
 		elsif content.class==Array and direction==:horizontal then
-			content.length.times {|i| @tileset[content[i]].draw((x+i)*16,y*16,0,1,1,color)}
+			content.length.times {|i| @tileset[content[i]].draw((x+i)*16,y*16,z_order,1,1,color)}
 		elsif content.class==Array and direction==:vertical then
-			content.length.times {|j| @tileset[content[j]].draw(x*16,(y+j)*16,0,1,1,color)}
+			content.length.times {|j| @tileset[content[j]].draw(x*16,(y+j)*16,z_order,1,1,color)}
 		end
 	end
 	
+	def draw_buffer(x,y,width,height,z_order)
+		# eventually this would draw the buffer where game data is dumped for drawing
+	end
+	
 	# draw a rectangular frame
-	def draw_frame(x,y,width,height,type=:double,color=0xFFFFFFFF)
+	def draw_frame(x,y,width,height,z_order,type=:double,color=0xFFFFFFFF)
 		case type
 			when :double then
 				tiles={:topleft => :table_topleft_double, :topright => :table_topright_double, :bottomright => :table_bottomright_double, :bottomleft => :table_bottomleft_double, :horizontal => :table_horizontal_double, :vertical => :table_vertical_double}
@@ -30,13 +34,13 @@ module Interface
 			when :heart then
 				tiles={:topleft => :heart, :topright => :heart, :bottomright => :heart, :bottomleft => :heart, :horizontal => :heart, :vertical => :heart}
 		end
-		draw_tiles(x,y,tiles[:topleft],:horizontal,color)
-		draw_tiles(x+width-1,y,tiles[:topright],:horizontal,color)
-		draw_tiles(x+width-1,y+height-1,tiles[:bottomright],:horizontal,color)
-		draw_tiles(x,y+height-1,tiles[:bottomleft],:horizontal,color)
-		draw_tiles(x+1,y,[tiles[:horizontal]]*(width-2),:horizontal,color)
-		draw_tiles(x+1,y+height-1,[tiles[:horizontal]]*(width-2),:horizontal,color)
-		draw_tiles(x,y+1,[tiles[:vertical]]*(height-2),:vertical,color)
-		draw_tiles(x+width-1,y+1,[tiles[:vertical]]*(height-2),:vertical,color)
+		draw_tiles(x,y,z_order,tiles[:topleft],:horizontal,color)
+		draw_tiles(x+width-1,y,z_order,tiles[:topright],:horizontal,color)
+		draw_tiles(x+width-1,y+height-1,z_order,tiles[:bottomright],:horizontal,color)
+		draw_tiles(x,y+height-1,z_order,tiles[:bottomleft],:horizontal,color)
+		draw_tiles(x+1,y,z_order,[tiles[:horizontal]]*(width-2),:horizontal,color)
+		draw_tiles(x+1,y+height-1,z_order,[tiles[:horizontal]]*(width-2),:horizontal,color)
+		draw_tiles(x,y+1,z_order,[tiles[:vertical]]*(height-2),:vertical,color)
+		draw_tiles(x+width-1,y+1,z_order,[tiles[:vertical]]*(height-2),:vertical,color)
 	end
 end
--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@ require 'tileset'
 require 'cut' # various snippets such as the new rand()
 
 class GameWindow < Gosu::Window
+	Background, Base, Foreground, Overlay = *0..3 # Z levels
 # advanced initialization, not really needed now
 #  def initialize(ini)
 #    x=ini['Display']['DisplayX']
@@ -13,27 +14,42 @@ class GameWindow < Gosu::Window
 #    super(x.to_i,y.to_i,z.to_i)
   
   include Interface
+  include Math
 
   def initialize
     super(1024,768,0)
     self.caption="Generic title"
     @tileset=Tileset.new(self)
-    @color=Gosu::Color.new(0xFFFFFFFF)
+    @color=Gosu::Color.new(255,255,0,0)
+    @hash={
+    [1,1] => [Base,:face_full,0xFF00FF00]
+    }
   end
   
   def update()
-	@timer=Gosu::milliseconds()/5
-	@color=Gosu::Color.new(255,0,@timer%255,0)
-
+	@color.red=128+(sin(Gosu::milliseconds.to_f/200)*127).to_i
+	rand(10).times do 
+		@hash[[rand(65),rand(48)]]=[Base,Tileset::SYMBOLS[rand(255)],rand(0xFFFF0000,0xFF00FF00,0xFF0000FF,0xFFFFFF00,0xFFFF00FF,0xFF00FFFF,0xFFFFFFFF)]
+	end
   end
   
   def draw()
-	  draw_frame(0,0,width/32,height/16,:double,0xFF00FFFF)
-	  draw_frame(32,0,32,10,:single,0xFFFFFFFF)
-	  draw_frame(32,10,32,10,:solid,0xFF0000FF)
-	  draw_frame(32,20,32,10,:heart,0xFFFF0000)
-	  draw_tiles(33,11,[:H,:e,:l,:l,:o,:space,:w,:o,:r,:l,:d],:horizontal,0xFFCCCCCC)
-	  draw_tiles(33,31,[:H,:e,:l,:l,:o,:space,:w,:o,:r,:l,:d],:vertical,@color)
+	  #draw_frame params: (x,y,width,height,Z,style,color) styles: :double, :single, :solid, :heart
+	  #draw_tiles params: (x,y,Z, symbol or array of symbols, :horizontal or :vertical, color)
+	  # Z order: Background, Base, Foreground, Overlay
+	  draw_frame(0,0,width/32,height/16,Background,:double,0xFFFFFFFF)
+	  draw_frame(32,0,32,10,Base,:single,0xFFFFFFFF)
+	  draw_frame(32,10,32,10,Foreground,:solid,0xFF0000FF)
+	  draw_frame(32,20,32,10,Foreground,:heart,@color)
+	  draw_tiles(33,11,Base,[:H,:e,:l,:l,:o,:space,:w,:o,:r,:l,:d],:horizontal,0xFFCCCCCC)
+	  draw_tiles(33,31,Base,[:H,:e,:l,:l,:o,:space,:w,:o,:r,:l,:d],:vertical,0xFFFFFFFF)
+	  draw_tiles(33,11,Background, [:fill100]*11,:horizontal,0x77FFFF00)
+	  draw_tiles((mouse_x.to_i-1)/16,(mouse_y.to_i-1)/16,Overlay,:fill100,:horizontal,0x88FFFFFF)
+	  @hash.each_pair do |key,value|
+			draw_tiles(key[0],key[1],value[0],value[1],:horizontal,value[2])
+			#value[0] = Z order, value[1] = symbol, value[2] = color
+		end
+		@hash.clear
   end
 end
 
