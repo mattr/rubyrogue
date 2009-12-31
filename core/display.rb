e431145df3d38a38294efb477cdcb8725ed1361c
mihey81 <mihey81@localhost>
2009-12-31 19:24:20 +0100

    Moved the tileset image to the new images folder.

--- core/display.rb
+++ core/display.rb
@@ -8,7 +8,7 @@ require './core/handler'
 require './core/misc'
 
 class Tileset < Gosu::Image
-  def Tileset.new(window, filename="cooz_16x16.png", symbols = SYMBOLS, alternate = ALTERNATE_SYMBOLS) # filenames are case sensitive on some OSes
+  def Tileset.new(window, filename="./images/Cooz_16x16.png", symbols = SYMBOLS, alternate = ALTERNATE_SYMBOLS) # filenames are case sensitive on some OSes
     tileset_array = Tileset.load_tiles(window, filename, 16, 16, 0)
     hash = Hash[*symbols.zip(tileset_array).flatten]
     alternate.each{|key, value| hash[key] = hash[value]}
